09ab2be63d1d44eec8bdb479f02717f4ee0a1e7f
Joel Ferreira <30007769@students.ual.pt>
2023-01-13 19:20:08 +0000

    addded checkout

--- client/src/pages/checkout/checkout.js
+++ client/src/pages/checkout/checkout.js
@@ -3,24 +3,50 @@ import React ,{useState}from 'react'
 export const CheckoutPage = ({cart}) => {
   return (
    <>
-   <div className="row">
-    <div className="col">
-        <div className="row">
-            <div className="col">
-                <h1> Dados</h1>
-            </div>
-        </div>
-    </div>
-    <div className="col">
-        <div className="row">
-            <div className="col">
-                <div className="card">
-                    <div className="card-body">
+   <div className="row d-flex justify-content-center ">
+    <div className="col col-sm-6">
+    <div className="card">
+                <h5 className="card-header">
+                          <div className="row">
+                                        <div className="col">                                            
+                                            <h5>Produt</h5>
+                                        </div>
+                                        <div className="col">                                            
+                                            <h5>Quantity</h5>
+                                        </div>
+                                        <div className="col">                                            
+                                            <h5>Price</h5>
+                                        </div>
+                                
+                                </div>     
                         
+                </h5>
+                    <div className="card-body">
+                       {cart.map((ele,index)=>{
+                            return <div key={index} className="row">
+                                <div className="col">
+                                    <img src={ele.imgUrl} height={100} alt="" />                             
+                                </div>
+                                <div className="col">
+                                    <h4 className='mt-4'> {ele.quantity}</h4>                               
+                                </div>
+                                <div className="col">
+                                    <h4 className='mt-4'>{ele.price} € </h4>                               
+                                </div>
+                            </div>
+                       })}
                     </div>
-                </div>
-            </div>
-        </div>
+                    <div class="card-footer text-muted">
+                        <div className="row">
+                            <div className="col d-flex justify-content-end">
+                                <div>
+                                            <button className="btn btn-dark w-100"> Make Purchase</button>
+                                </div>
+                            </div>
+                        </div>
+                      
+                    </div>
+     </div>
     </div>
    </div>
    </>
