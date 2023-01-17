48f684ddeb44a025557584936b02201202dad23a
Joel Ferreira <30007769@students.ual.pt>
2023-01-17 20:37:06 +0000

    bug fix on case token was invalid

--- client/src/App.js
+++ client/src/App.js
@@ -31,18 +31,25 @@ function App() {
  async  function getCart(){
   const token=localStorage.getItem("token");
   if(token){
-     const response= await fetch("http://localhost:3001/api/cart",{
-      method:"GET",
-      headers:{
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      }
-
-     })
-     const userCart= await response.json();
-     setCart(userCart);
-     getTotal(userCart);
-     setCart(userCart);
+    try {
+      const response= await fetch("http://localhost:3001/api/cart",{
+        method:"GET",
+        headers:{
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+  
+       })
+       const userCart= await response.json();
+       setCart(userCart);
+       getTotal(userCart);
+       setCart(userCart);
+      
+    } catch (error) {
+      localStorage.removeItem("token")
+      setCart(null);
+      setCart(null);
+    }
   }
    
   }
